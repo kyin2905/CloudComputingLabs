a237b4feb137867f33bdf6793d1788aaec19383e
jliudgx <105218965+jliudgx@users.noreply.github.com>
2023-04-11 22:20:23 +0800

    update main.cc

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -2,56 +2,120 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
+#include <thread>
+#include <pthread.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
 
 #include "sudoku.h"
 
-int64_t now()
+using namespace std;
+
+const int lim_puzzle = 1024;
+
+const int max_thread_num = 100;
+
+int thread_num = 0;
+
+int **buf;
+bool *is_solved;
+
+string infile_buffer;
+
+thread ths[max_thread_num];
+char puzzle[128];
+
+void init_before_all()
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec * 1000000 + tv.tv_usec;
+    // ios::sync_with_stdio(false);
+    buf = new int* [lim_puzzle];
+    is_solved = new bool [lim_puzzle];
+    for(int i = 0; i < lim_puzzle; ++i) buf[i] = new int [N];
+    thread_num = thread::hardware_concurrency();
 }
 
-int main(int argc, char* argv[])
+void init_before_one_round()
 {
-  init_neighbors();
-
-  FILE* fp = fopen(argv[1], "r");
-  char puzzle[128];
-  int total_solved = 0;
-  int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
-      solve = solve_sudoku_min_arity;
-    else if (argv[2][0] == 'c')
-      solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
-  int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-    if (strlen(puzzle) >= N) {
-      ++total;
-      input(puzzle);
-      init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
-      if (solve(0)) {
-        ++total_solved;
-        if (!solved())
-          assert(0);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
+    for(int i = 0; i < lim_puzzle; i++)
+    {
+        is_solved[i] = false;
     }
-  }
-  int64_t end = now();
-  double sec = (end-start)/1000000.0;
-  printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
-
-  return 0;
 }
 
+void thread_work(const int sta, const int line_num)
+{
+    for(int i = 0; i < line_num; ++i)
+    {
+        is_solved[sta+i] = solve_sudoku_dancing_links(buf[sta+i]);
+    }
+}
+
+
+void one_file_work(string file_path)
+{
+    ifstream input_file(file_path, ios::in);
+    if(!input_file.is_open())
+    {
+        // printf("Filed to open file: %s\n", file_path.c_str());
+        return;
+    }
+    init_before_one_round(); 
+    bool end_of_file = false;
+    while(!end_of_file)
+    {
+          int line_count = 0;
+          do{
+            //  char* ret_gets = fgets(puzzle, sizeof puzzle, fp);
+             if(input_file.eof())
+             {
+                end_of_file = true;
+                break;
+             }
+             input_file.getline(puzzle, N+1);
+             if (strlen(puzzle) >= N)
+             {
+                trans(puzzle, buf[line_count]);
+                line_count++;
+             }
+          }while(line_count < lim_puzzle-1);
+//------------------------------------------------------------------
+//          for(int i = 0; i < line_count; ++i)
+//          {
+//              is_solved[i] = solve_sudoku_dancing_links(buf[i]);
+//          }
+          int step = (line_count+thread_num-1)/thread_num, curr = 0;
+          for(int i = 0; i < thread_num; ++i,curr+=step)
+          {
+              ths[i] = thread(thread_work, curr, ((curr+step>=line_count)?(line_count-curr):step));
+          }
+          for(int i = 0; i < thread_num; ++i) ths[i].join();
+
+//------------------------------------------------------------------
+          for(int i = 0; i < line_count; ++i)
+          {
+              if(is_solved[i])
+              {
+                 for(int j = 0; j < N; j++) putchar('0'+buf[i][j]);
+                 putchar('\n');
+              }
+              else puts("No result.");
+          }
+    }
+    input_file.close();
+}
+
+int main()
+{
+    // init
+    init_before_all();
+    string file_path;
+    while(getline(cin, file_path))
+    {
+        one_file_work(file_path);
+    }
+    for(int i = 0; i < lim_puzzle; ++i) delete [] buf[i];
+    delete [] buf;
+    return 0;
+}
